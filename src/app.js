f39962320030103801d29b1ba3eabc332a7fe129
Juan Baltazar <juan.baltazar1@gmail.com>
2021-06-28 23:31:48 -0700

    changed cors

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ let corsOptionsDelegate = function (req, callback) {
   if (whiteList.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { origin: false } // disable CORS for this request
+    corsOptions = { origin: true } // disable CORS for this request
   }
   callback(null, corsOptions) // callback expects two parameters: error and options
 }
